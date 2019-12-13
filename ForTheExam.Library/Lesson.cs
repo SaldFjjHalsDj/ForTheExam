3eab8788db6cd7e8b99d9d9640616c880596d5a0
Andrew Lobov <lobovav@live.com>
2019-12-13 17:17:31 +0300

    # Возможный вариант структуры данных по экзаменам.

---
+++ ForTheExam.Library/Lesson.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ForTheExam.Library
+{
+    public class Lesson
+    {
+        public string Name { get; set; }
+
+        public AttestationType AttestationType { get; set; }
+
+        public bool IsPassed { get; set; }
+        public int Mark { get; set; }
+
+    }
+}
